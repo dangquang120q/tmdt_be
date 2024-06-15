8dd4aff146bddfa88e0bdaf87abfe4fa94ff9013
qquynh217 <nltquynh875@gmail.com>
2024-06-15 07:53:17 +0700

    update: chageFeedbackReply

--- config/routes.js
+++ config/routes.js
@@ -80,6 +80,7 @@ module.exports.routes = {
   "POST /staff/productStatistics": "StaffController.productStatistics",
   "POST /staff/productLineStatistics": "StaffController.productLineStatistics",
   "POST /staff/getListFeedback": "StaffController.getListFeedback",
+  "POST /staff/changeFeedbackReply": "StaffController.changeFeedbackReply",
 
   /***************************************************************************
    *                                                                          *
